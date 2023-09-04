7cde792d477a1a4c730c6d8df1d4b8acd4d3d29d
Jsanchez32 <juanandressanchezrey@gmail.com>
2023-09-04 06:39:51 -0500

    feat: :sparkles: Documentacion

--- index.js
+++ index.js
@@ -19,18 +19,24 @@ const writeFilePro = (file,data)=>{
     })
 }
 
+//Creamos 2 array vacios para luego llenarlos//
 let img = []
 let dogs = []
 const getDog = async ()=>{
     try {
         const data = await readFilePro(`${__dirname}/dog.txt`);
+        //Agregamos los datos del txt al array//
         dogs.push(data)
+        //Usando el metodo splip separamos los elementos del array por medio de la ","//
         const dog = dogs[0].split(',') ;
 
         console.log(dog);
+        //Usamos un for para recorrer cada perro y que de esa manera la api lea todos//
         for (let i = 0; i < dog.length; i++) {
             const res = await superagent.get(`https://dog.ceo/api/breed/${dog[i]}/images/random`);
+            //Guardamos en el array img las urls de los perros//
             img.push(res.body.message)
+            //Usamos el join para q no se remplazen los archivos y el "\n" para que baje una linea de texto//
             await writeFilePro('dog-img.txt', img.join('\n'));            
         }
         console.log(img);
